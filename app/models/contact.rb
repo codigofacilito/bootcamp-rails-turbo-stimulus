c5709a511a7f6699b4ed6aee23667e3ce1a90f71
Uriel Hernández <uriel.hdzc@gmail.com>
2022-07-08 10:14:55 -0500

    :D

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,5 @@ class Contact < ApplicationRecord
   has_one :person
   has_many :phone_numbers
   
-  accepts_nested_attributes_for :phone_numbers, allow_destroy: true
-  accepts_nested_attributes_for :person, allow_destroy: true
   
 end
